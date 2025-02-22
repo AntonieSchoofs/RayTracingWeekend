3247b59a42cdd6ab81361ea36e1a76435565c01d
Antonie Schoofs <antonieschoofs@gmail.com>
2025-02-22 21:51:37 +0100

    Chapter 9

--- color.h
+++ color.h
@@ -4,11 +4,24 @@
 #include "interval.h"
 #include "vec3.h"
 
+inline double linear_to_gamma(double linear_component)
+{
+    if (linear_component > 0)
+        return std::sqrt(linear_component);
+
+    return 0;
+}
+
 void write_color(std::ostream &out, const color &pixel_color)
 {
     auto r = pixel_color.x();
     auto g = pixel_color.y();
     auto b = pixel_color.z();
+
+    // Transform linear colors to gamma
+    r = linear_to_gamma(r);
+    g = linear_to_gamma(g);
+    b = linear_to_gamma(b);
 
     // std::cerr << "Color values: r=" << r << ", g=" << g << ", b=" << b << std::endl;
     //  Translate the [0,1] component values to the byte range [0,255].
